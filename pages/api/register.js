ba91cc504d849ffea177c875a701ba7610903d18
tacotacoenchilada <74435721+tacotacoenchilada@users.noreply.github.com>
2022-05-28 20:58:22 -0400

    gay

---
+++ pages/api/register.js
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+export default async function handle(req, res) {
+  // console.log(req.body);
+  await prisma.$connect();
+  if(await prisma.user.findFirst({where: { email: req.body.email}})) {
+    res.status(404).json({error: 'User With Email Found'})
+  }else {
+  await prisma.user.create({
+   data: {
+     name: req.body.name,
+      email: req.body.email,
+    }
+  }).then((resp) => {
+    // console.log(resp);
+     res.status(200).json(JSON.stringify({name: resp.name, email: resp.email}))
+  })
+}
+}
